232212218c867b7113ccf9d96bb034acc32ef7be
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-01-10 16:29:11 +0000

    Created a JS file and added my first question

---
+++ Developing /script.js
@@ -0,0 +1,12 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who invented JavaScript?",
+    answer: "Brendan Eich",
+    options: [
+      "Douglas Crockford",
+      "Sheryl Sandberg",
+      "Brendan Eich",
+      "Harry Maguire"
+    ]
