6d4fdeecc8e7b7c5b36043f6280d84e727441151
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-01-10 16:33:27 +0000

    Created a function for the timer

--- Developing /script.js
+++ Developing /script.js
@@ -68,3 +68,14 @@ var newHS = 0;
 start_btn.onclick = ()=>{
     info_box.classList.add("activeInfo"); //show info box
 }
+
+// if continueQuiz button clicked
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide info box
+    quiz_box.classList.add("activeQuiz"); //show quiz box
+    showQuetions(0); //calling showQestions function
+    queCounter(1); //passing 1 parameter to queCounter
+    startTimer(60); //calling startTimer function
+    startTimerLine(0); //calling startTimerLine function
+}
+
